0cdf51c87aefc87ccf7519d2628974447d7ed2bb
strivegfx <notifydevon@gmail.com>
2015-12-14 23:05:45 +1300

    Added in the relevance check for the triangle shape in the icons resting state

--- src/js/movement.js
+++ src/js/movement.js
@@ -20,17 +20,18 @@ const Movement = class {
 
 		for (let i = 0; i < this.Dots.total; i += 1) {
 
-			// const increment = 3;
-			// instances[i].x = this.Pin.Helper.boolean() ? instances[i].x += increment : instances[i].x -= increment;
-			// instances[i].y = this.Pin.Helper.boolean() ? instances[i].y += increment : instances[i].y -= increment;
-
-			// const properties = this.updateAngle(instances[i]);
-			// console.log(properties);
-			// instances[i].angle = properties.angle;
-			// instances[i].x = properties.x;
-			// instances[i].y = properties.y;
-
 			const properties = this.getNewCoordinates(instances[i]);
+
+			/*
+			 movement: {
+			 	angle = 145, // 0 --> 360
+			 	steps = 6, // 30 --> 120
+				direction: left, // hardLeft, left, straight, right, hardRight
+				speed: 2 // 1 --> 3
+		 	};
+
+			*/
+
 			instances[i].x = properties.x;
 			instances[i].y = properties.y;
 
@@ -55,14 +56,10 @@ const Movement = class {
 
 
 		} while(!this.Pin.Ring.testRelevance(hypotenuse)  &&
-				!this.Pin.Circle.testRelevance(hypotenuse));
+				!this.Pin.Circle.testRelevance(hypotenuse) &&
+				!this.Pin.Triangle.testRelevance(x, y));
 
 		return {x, y};
-
-		// ctx.beginPath();
-		// ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-		// ctx.fillStyle = red;
-		// ctx.fill();
 
 	}
 
@@ -76,267 +73,126 @@ const Movement = class {
 
 	}
 
-
-
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-
-	updateAngle(instance) {
-
-		// let angle = instance.angle;
-		const offset = this.calculateOffset();
-			// console.log(`offset = ${offset}`);
-		const angle = this.rebase360(instance.angle, offset);
-			// console.log(`angle = ${angle}`);
-		const quadrant = this.quadrantProperties(angle);
-			// console.log(`quadrant = ${quadrant}`);
-		const trajectory = this.calculateTrajectory(angle, quadrant);
-			// console.log(`trajectory = ${trajectory}`);
-
-		const coordinates = this.updateCoordinates(instance, quadrant, trajectory);
-
-		return {
-			angle: angle,
-			x: coordinates.x,
-			y: coordinates.y
-		};
-
-		// calculateOffset
-		// rebase360
-		// quadrantProperties
-		// calculateTrajectory
-		// updateCoordinates
-
-
-
-
-
-	}
-
-	calculateOffset() {
-
-		const max = 1;
-		const offset = this.Pin.Helper.randomise({max});
-
-		return offset;
-
-	}
-
-	rebase360(angle, offset) {
-
-		console.log(angle, offset);
-
-		angle = this.Pin.Helper.boolean() ? angle += offset : angle -= offset;
-
-		// return angle < 0 ? 360 - (angle * -1) : angle > 360 ? angle - 360 : angle;
-
-		if (angle < 0) {
-
-			angle = 360 - (angle * -1);
-
-		} else if (angle > 360) {
-
-			angle = angle - 360;
-
-		}
-
-		return angle;
-
-	}
-
-
-	quadrantProperties(angle) {
-
-		let properties = {};
-
-		if (angle > 0 && angle <= 90) {
-
-			// console.log('1 -- 90');
-			properties.location = 0;
-			properties.positiveX = true;
-			properties.positiveY = true;
-			properties.invert = true;
-
-		} else if (angle > 90 && angle <= 180) {
-
-			// console.log('91 -- 180');
-			properties.location = 1;
-			properties.positiveX = true;
-			properties.positiveY = false;
-			properties.invert = false;
-
-		} else if (angle > 180 && angle <= 270) {
-
-			// console.log('181 -- 270');
-			properties.location = 2;
-			properties.positiveX = false;
-			properties.positiveY = false;
-			properties.invert = true;
-
-		} else { // 271 -- 360 / 0
-
-			// console.log('271 -- 360 / 0');
-			properties.location = 3;
-			properties.positiveX = false;
-			properties.positiveY = true;
-			properties.invert = false;
-
-		}
-
-		return properties;
-
-	}
-
-	calculateTrajectory(angle, quadrant) {
-
-		let trajectory = angle - (quadrant.location * 90);
-
-		return quadrant.invert ? 90 - trajectory : trajectory;
-
-	}
-
-	updateCoordinates(instance, quadrant, trajectory) {
-
-		const width = Math.cos(trajectory) * 2;
-		const height = Math.sin(trajectory) * 2;
-		const x = quadrant.positiveX ? instance.x +=  width : instance.x -=  width;
-		const y = quadrant.positiveY ? instance.y +=  height : instance.y -=  height;
-
-		// console.log(`triangle = d(${trajectory}), w(${width}), h(${height}), x(${x}), y(${y})`);
-
-		return {x, y};
-
-		// instance.x = x;
-		// instance.y = y;
-
-	}
-
 };
 
 module.exports = Movement;
 
-
-/*
-
-var $canvas = $('#icon');
-var ctx = $canvas[0].getContext('2d');
-var black = 'rgb(0, 0, 0)';
-var white = 'rgb(255, 255, 255)';
-var red = 'rgb(255, 0, 0)';
-window.requestAnimationFrame(manipulateDots);
-
-function randomise(min, max) {
-
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-
-}
-
-function testCircleRelevance(x, y) {
-
-	var width = x > 150 ? x - 150 : 150 - x;
-	var height = y > 150 ? y - 150 : 150 - y;
-	var radius = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-
-	// console.log(`width = ${width} | height = ${height} | radius = ${radius}`);
-
-	return radius < 150 && radius > 50 ? true : false;
-
-}
-
-function testPointRelevance(x, y) {
-
-	// var pointMatch = (Math.pow(10, 2) - Math.pow(5, 2));
-	// console.log(pointMatch);
-	// console.log(75 / 100);
-
-	// 1.73205081
-
-	// are we in the squares vicinity
-	if ((x < 150 - 40 || 150 + 40) && (y < 280 || y > 340)) { return; }
-
-	var width = x > 150 ? (150 + 40) - x : x - (150 - 40); // x - (150 - 40) : (150 + 40) - x;
-	var height = y - 280;
-	var ratio = 1.73205081;
-	// var length = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-
-	if (width * ratio > height) {
-		console.log(`x = ${x} | y = ${y} | width = ${width} | height = ${height} | ratio = ${width * ratio}`);
-
-		// var offset = x > 150 ? x - width : x + width;
-		// ctx.beginPath();
-		// ctx.moveTo(x, y);
-		// ctx.lineTo(x, 280);
-		// ctx.lineTo(offset, 280);
-		// ctx.lineTo(x, y);
-		// ctx.strokeStyle = red;
-		// ctx.stroke();
-	}
-
-	return width * ratio > height ? true : false; // length < pointMatch ? true : false;
-
-}
-
-function placeDot() {
-
-	var radius = 5;
-	var x;
-	var y;
-
-	do {
-
-		x = randomise(0, 300) - radius;
-		y = randomise(0, 350) - radius;
-
-	} while(!testCircleRelevance(x, y) && !testPointRelevance(x, y));
-
-	ctx.beginPath();
-	ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-	ctx.fillStyle = red;
-	ctx.fill();
-
-}
-
-function manipulateDots() {
-
-	ctx.clearRect(0,0,300,350);
-
-	for (var i = 0; i < 1000; i += 1) {
-
-		placeDot();
-
-	}
-
-	// buildStencil();
-
-	window.requestAnimationFrame(manipulateDots);
-
-}
-
-function buildStencil() {
-
-	ctx.beginPath();
-	ctx.arc(150, 150, 150, 0, Math.PI * 2, true);
-	ctx.strokeStyle = black;
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.arc(150, 150, 50, 0, Math.PI * 2, true);
-	ctx.strokeStyle = black;
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.moveTo(150 - 40, 280);
-	ctx.lineTo(150, 340);
-	ctx.lineTo(150 + 40, 280);
-	ctx.strokeStyle = black;
-	ctx.stroke();
-
-}
-
-*/
+// updateAngle(instance) {
+//
+// 	// let angle = instance.angle;
+// 	const offset = this.calculateOffset();
+// 		// console.log(`offset = ${offset}`);
+// 	const angle = this.rebase360(instance.angle, offset);
+// 		// console.log(`angle = ${angle}`);
+// 	const quadrant = this.quadrantProperties(angle);
+// 		// console.log(`quadrant = ${quadrant}`);
+// 	const trajectory = this.calculateTrajectory(angle, quadrant);
+// 		// console.log(`trajectory = ${trajectory}`);
+//
+// 	const coordinates = this.updateCoordinates(instance, quadrant, trajectory);
+//
+// 	return {
+// 		angle: angle,
+// 		x: coordinates.x,
+// 		y: coordinates.y
+// 	};
+//
+// }
+//
+// calculateOffset() {
+//
+// 	const max = 1;
+// 	const offset = this.Pin.Helper.randomise({max});
+//
+// 	return offset;
+//
+// }
+//
+// rebase360(angle, offset) {
+//
+// 	console.log(angle, offset);
+//
+// 	angle = this.Pin.Helper.boolean() ? angle += offset : angle -= offset;
+//
+// 	// return angle < 0 ? 360 - (angle * -1) : angle > 360 ? angle - 360 : angle;
+//
+// 	if (angle < 0) {
+//
+// 		angle = 360 - (angle * -1);
+//
+// 	} else if (angle > 360) {
+//
+// 		angle = angle - 360;
+//
+// 	}
+//
+// 	return angle;
+//
+// }
+//
+//
+// quadrantProperties(angle) {
+//
+// 	let properties = {};
+//
+// 	if (angle > 0 && angle <= 90) {
+//
+// 		// console.log('1 -- 90');
+// 		properties.location = 0;
+// 		properties.positiveX = true;
+// 		properties.positiveY = true;
+// 		properties.invert = true;
+//
+// 	} else if (angle > 90 && angle <= 180) {
+//
+// 		// console.log('91 -- 180');
+// 		properties.location = 1;
+// 		properties.positiveX = true;
+// 		properties.positiveY = false;
+// 		properties.invert = false;
+//
+// 	} else if (angle > 180 && angle <= 270) {
+//
+// 		// console.log('181 -- 270');
+// 		properties.location = 2;
+// 		properties.positiveX = false;
+// 		properties.positiveY = false;
+// 		properties.invert = true;
+//
+// 	} else { // 271 -- 360 / 0
+//
+// 		// console.log('271 -- 360 / 0');
+// 		properties.location = 3;
+// 		properties.positiveX = false;
+// 		properties.positiveY = true;
+// 		properties.invert = false;
+//
+// 	}
+//
+// 	return properties;
+//
+// }
+//
+// calculateTrajectory(angle, quadrant) {
+//
+// 	let trajectory = angle - (quadrant.location * 90);
+//
+// 	return quadrant.invert ? 90 - trajectory : trajectory;
+//
+// }
+//
+// updateCoordinates(instance, quadrant, trajectory) {
+//
+// 	const width = Math.cos(trajectory) * 2;
+// 	const height = Math.sin(trajectory) * 2;
+// 	const x = quadrant.positiveX ? instance.x +=  width : instance.x -=  width;
+// 	const y = quadrant.positiveY ? instance.y +=  height : instance.y -=  height;
+//
+// 	// console.log(`triangle = d(${trajectory}), w(${width}), h(${height}), x(${x}), y(${y})`);
+//
+// 	return {x, y};
+//
+// 	// instance.x = x;
+// 	// instance.y = y;
+//
+// }
--- src/js/triangle.js
+++ src/js/triangle.js
@@ -1,7 +1,5 @@
 const $ = require('jquery');
 
-// location pin
-
 const Triangle = class {
 
 	constructor(Pin) {
@@ -12,13 +10,12 @@ const Triangle = class {
 		this.size = 84;
 		this.x = this.Pin.center - (this.size / 2);
 		this.y = this.Pin.center + 130;
-		this.ratio = this.generateRatio();
-
-		this.generateStencil(); // for debuging ONLY
+		this.ratio = this.calculateRatio();
+		this.height = this.size / 2 * this.ratio + this.y;
 
 	}
 
-	generateRatio() {
+	calculateRatio() {
 
 		// Find the ratio of an equilateral triangles incline in respect to its
 		// width and height properties.
@@ -61,126 +58,44 @@ const Triangle = class {
 
 	}
 
+	testRelevance(x, y) {
+
+		// We need to find if the current dot instance resides within the
+		// triangle area. First we test if the square that surrounds the
+		// triangle has any relevance and if so we move onto calculating the
+		// dots relationship to the triangles actual dimensions.
+		//
+		// We once again cut the equilateral triangle in half to create an easy
+		// to manage right angle triangle. We want to make a right angle
+		// triangle out of the current x and y coordinates and reference it
+		// against the slope of the actual triangle shape (a larger slope ===
+		// outside the triangle area).
+		//
+		// To create the test we set the triangle to be in either the top left
+		// or top right corner of the icon triangle (depending on which is
+		// closer i.e. which side of the centre line the dot resides). Using
+		// this corner point and the dots location we can drive a width, height
+		// and starting point for the reference triangle.
+		//
+		// From there we use the equilateral ratio to test if the reference
+		// triangle height is > or < an equilateral height.
+
+		let center = this.Pin.center,
+			offset = this.size / 2;
+
+		if ((x < center - offset || x > center + offset) || (y < this.y || y > this.height)) { return false; }
+
+		// console.log(`Inside square -> x = ${x} vs (${center - offset} || ${center + offset} = ${x < center - offset && x > center + offset}) -> y = ${y} vs (${this.y} && ${this.height} ${y > this.y && y < this.height})`);
+
+		let width = x > center ? (center + offset) - x : x - (center - offset),
+			height = y - this.y;
+
+		// console.log(`width: ${width} (${center + offset} - ${x}) | height: ${height} (${y} - ${this.y})`);
+
+		return width * this.ratio > height ? true : false;
+
+	}
+
 };
 
 module.exports = Triangle;
-
-
-/*
-
-var $canvas = $('#icon');
-var ctx = $canvas[0].getContext('2d');
-var black = 'rgb(0, 0, 0)';
-var white = 'rgb(255, 255, 255)';
-var red = 'rgb(255, 0, 0)';
-window.requestAnimationFrame(manipulateDots);
-
-function randomise(min, max) {
-
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-
-}
-
-function testCircleRelevance(x, y) {
-
-	var width = x > 150 ? x - 150 : 150 - x;
-	var height = y > 150 ? y - 150 : 150 - y;
-	var radius = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-
-	// console.log(`width = ${width} | height = ${height} | radius = ${radius}`);
-
-	return radius < 150 && radius > 50 ? true : false;
-
-}
-
-function testPointRelevance(x, y) {
-
-	// var pointMatch = (Math.pow(10, 2) - Math.pow(5, 2));
-	// console.log(pointMatch);
-	// console.log(75 / 100);
-
-	// 1.73205081
-
-	// are we in the squares vicinity
-	if ((x < 150 - 40 || 150 + 40) && (y < 280 || y > 340)) { return; }
-
-	var width = x > 150 ? (150 + 40) - x : x - (150 - 40); // x - (150 - 40) : (150 + 40) - x;
-	var height = y - 280;
-	var ratio = 1.73205081;
-	// var length = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
-
-	if (width * ratio > height) {
-		console.log(`x = ${x} | y = ${y} | width = ${width} | height = ${height} | ratio = ${width * ratio}`);
-
-		// var offset = x > 150 ? x - width : x + width;
-		// ctx.beginPath();
-		// ctx.moveTo(x, y);
-		// ctx.lineTo(x, 280);
-		// ctx.lineTo(offset, 280);
-		// ctx.lineTo(x, y);
-		// ctx.strokeStyle = red;
-		// ctx.stroke();
-	}
-
-	return width * ratio > height ? true : false; // length < pointMatch ? true : false;
-
-}
-
-function placeDot() {
-
-	var radius = 5;
-	var x;
-	var y;
-
-	do {
-
-		x = randomise(0, 300) - radius;
-		y = randomise(0, 350) - radius;
-
-	} while(!testCircleRelevance(x, y) && !testPointRelevance(x, y));
-
-	ctx.beginPath();
-	ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-	ctx.fillStyle = red;
-	ctx.fill();
-
-}
-
-function manipulateDots() {
-
-	ctx.clearRect(0,0,300,350);
-
-	for (var i = 0; i < 1000; i += 1) {
-
-		placeDot();
-
-	}
-
-	// buildStencil();
-
-	window.requestAnimationFrame(manipulateDots);
-
-}
-
-function buildStencil() {
-
-	ctx.beginPath();
-	ctx.arc(150, 150, 150, 0, Math.PI * 2, true);
-	ctx.strokeStyle = black;
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.arc(150, 150, 50, 0, Math.PI * 2, true);
-	ctx.strokeStyle = black;
-	ctx.stroke();
-
-	ctx.beginPath();
-	ctx.moveTo(150 - 40, 280);
-	ctx.lineTo(150, 340);
-	ctx.lineTo(150 + 40, 280);
-	ctx.strokeStyle = black;
-	ctx.stroke();
-
-}
-
-*/
